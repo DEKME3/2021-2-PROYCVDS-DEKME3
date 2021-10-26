0ed1fefafd0e7c4d2316342c291758db9eb2a786
Kristhian David Segura <kristhian.segura@mail.escuelaing.edu.co>
2021-10-25 19:40:56 -0500

    Clase usuario

---
+++ src/main/java/edu/eci/cvds/entities/UserType.java
@@ -0,0 +1,7 @@
+package edu.eci.cvds.entities;
+
+import java.io.Serializable;
+
+public class UserType implements Serializable{
+    
+}
--- src/main/java/edu/eci/cvds/entities/Usuario.java
+++ src/main/java/edu/eci/cvds/entities/Usuario.java
@@ -1,5 +1,12 @@
 package edu.eci.cvds.entities;
 
-public class Usuario {
-    
+import java.io.Serializable;
+
+public class Usuario implements Serializable{
+    private int id;
+    private String name;
+    private String password;
+    private String email;
+    private boolean status;
+    private UserType userType;
 }
