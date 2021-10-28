ee854c6a7559a699742fd1efe48523e1d5a399be
Deivid Medina <deivid.medina@mail.escuelaing.edu.co>
2021-10-28 08:30:31 -0500

    Creando Persistencia.

--- src/main/java/edu/eci/cvds/dao/mybatis/mappers/UserMapper.java
+++ src/main/java/edu/eci/cvds/dao/mybatis/mappers/UserMapper.java
@@ -3,10 +3,10 @@ package edu.eci.cvds.dao.mybatis.mappers;
 import org.apache.ibatis.annotations.Param;
 
 import edu.eci.cvds.entities.User;
+import edu.eci.cvds.entities.UserType;
 
 public interface UserMapper {
 
     public void InsertUser(@Param("user") User user);
     public User getUser(@Param("id") int id);
-    
 }
