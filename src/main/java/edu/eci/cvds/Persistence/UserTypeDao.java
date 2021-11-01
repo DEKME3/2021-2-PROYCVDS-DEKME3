03cb8805574ce433d04a1daf20130ac061b319c8
Deivid Medina <deivid.medina@mail.escuelaing.edu.co>
2021-11-01 16:50:57 -0500

    Eliminando Proyecto

--- src/main/java/edu/eci/cvds/Persistence/UserTypeDao.java
+++
@@ -1,9 +0,0 @@
-package edu.eci.cvds.Persistence;
-
-import edu.eci.cvds.entities.UserType;
-import edu.eci.cvds.exeptions.excepciones;
-
-public interface UserTypeDao {
-	
-	public UserType getUserType(int id) throws excepciones;
-}
--- src/main/java/edu/eci/cvds/services/UserServices.java
+++
@@ -1,9 +0,0 @@
-package edu.eci.cvds.services;
-
-import edu.eci.cvds.entities.User;
-
-public interface UserServices {
-    
-    public User getUser(String name); 
-    public boolean validateLogin(String name, String password);
-}
