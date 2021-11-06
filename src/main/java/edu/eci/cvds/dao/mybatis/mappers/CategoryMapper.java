91a1f67897774cef5ab7f76f22782c1a18660b89
Kristhian David Segura <kristhian.segura@mail.escuelaing.edu.co>
2021-11-06 15:43:04 -0500

    Need

--- src/main/java/edu/eci/cvds/dao/mybatis/mappers/CategoryMapper.java
+++ src/main/java/edu/eci/cvds/dao/mybatis/mappers/CategoryMapper.java
@@ -6,6 +6,6 @@ import edu.eci.cvds.entities.Category;
 public interface CategoryMapper {
 
     public void InsertCategory(@Param("category") Category category);
-    public Category getCategory(@Param("name") String name);
+    public Category getCategory(@Param("id") int id);
     public void ActualizarCategory(@Param("id") int id,@Param("name") String name, @Param("description") String description, @Param("status") String status);
 }
